f1034edb0727f58d78886a0b701e0e5be9aa80ab
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-05-30 14:23:16 +0300

    front end added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const baseUrl = "http://localhost:3001/api/notes";
 app.use(express.json());
-
+app.use(express.static("build"));
 const cors = require("cors");
 
 app.use(cors());
